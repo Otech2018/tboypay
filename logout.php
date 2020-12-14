9bd24fc6c62ff53ef21725acd68c25278566fd9b
Otech2018 <ajahogonnaya2012@gmail.com>
2020-12-14 20:59:11 +0100

    logout set

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 				session_start();
 					$_SESSION['Admin_id']="";
 				$_SESSION['user_id']="";
-			echo "<script>window.location.replace('index.html');</script>"; 
+			echo "<script>window.location.replace('index.php');</script>"; 
 
 
 
